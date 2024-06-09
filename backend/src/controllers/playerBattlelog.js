c72c39cf1f8cc7ca1654e8fb9260c2d4d2cb1e21
KiruSIVAN <kirusivan@gmail.com>
2024-06-09 13:57:51 +0200

    Most Used Deck from 25 matchs

--- backend/src/controllers/playerBattlelog.js
+++ backend/src/controllers/playerBattlelog.js
@@ -1,27 +1,40 @@
-const axios = require('axios');
+const axios = require("axios");
 
 // Appel API pour récupérer les données battlelog
 const getPlayerBattles = async (playertag) => {
   try {
-    const response = await axios.get(`https://api.clashroyale.com/v1/players/%23${playertag}/battlelog`, {
-      headers: {
-        Authorization: `Bearer ${process.env.API_KEY}`
+    const response = await axios.get(
+      `https://api.clashroyale.com/v1/players/%23${playertag}/battlelog`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
       }
-    });
+    );
 
     const battles = response.data.slice(0, 25); // Limit to the last 25 matches
     console.log(`Retrieved ${battles.length} battles for player ${playertag}`);
     return battles;
-    
   } catch (error) {
     if (error.response) {
-      console.error(`Error retrieving battlelog for player ${playertag}:`, error.response.data);
-      throw new Error(`Failed to retrieve battlelog: ${error.response.data.message}`);
+      console.error(
+        `Error retrieving battlelog for player ${playertag}:`,
+        error.response.data
+      );
+      throw new Error(
+        `Failed to retrieve battlelog: ${error.response.data.message}`
+      );
     } else if (error.request) {
-      console.error(`No response received for player ${playertag}:`, error.request);
-      throw new Error('Failed to retrieve battlelog: No response received');
+      console.error(
+        `No response received for player ${playertag}:`,
+        error.request
+      );
+      throw new Error("Failed to retrieve battlelog: No response received");
     } else {
-      console.error(`Error setting up request for player ${playertag}:`, error.message);
+      console.error(
+        `Error setting up request for player ${playertag}:`,
+        error.message
+      );
       throw new Error(`Failed to retrieve battlelog: ${error.message}`);
     }
   }
