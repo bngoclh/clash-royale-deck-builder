bbe6137e121e054d6298f836f10162997c486f1f
KiruSIVAN <kirusivan@gmail.com>
2024-06-10 17:26:25 +0200

    Added winRate

--- backend/src/controllers/mostUsedDeck.js
+++ backend/src/controllers/mostUsedDeck.js
@@ -1,46 +1,65 @@
 const findMostUsedDeck = (battles) => {
   const deckUsage = {}; // Dictionnaire pour compter l'utilisation de chaque deck
 
-  // Parcourt chaque bataille pour analyser les decks utilisés
+  // Parcourt chaque bataille pour analyser les decks utilisés et les résultats des matchs
   battles.forEach((battle) => {
     if (battle.team && battle.team.length > 0) {
       // Crée une représentation unique du deck en triant les noms des cartes
       const deck = battle.team[0].cards
         .map((card) => card.name)
         .sort()
-        .join(", ");
+        .join(",");
+      // Calcule le coût moyen en élixir du deck
       const elixirSum = battle.team[0].cards.reduce(
         (acc, card) => acc + card.elixirCost,
         0
       );
       const averageElixir = elixirSum / battle.team[0].cards.length;
+      // Détermine si la bataille est une victoire
+      const victory = battle.team[0].crowns > battle.opponent[0].crowns;
 
       // Si le deck n'a pas encore été rencontré, initialise ses valeurs
       if (!deckUsage[deck]) {
-        deckUsage[deck] = { count: 0, elixir: averageElixir };
+        deckUsage[deck] = {
+          count: 0,
+          elixir: averageElixir,
+          wins: 0,
+          losses: 0,
+        };
       }
 
       // Incrémente le compteur d'utilisation pour ce deck
       deckUsage[deck].count += 1;
+      // Incrémente le compteur de victoires ou de défaites pour ce deck
+      if (victory) {
+        deckUsage[deck].wins += 1;
+      } else {
+        deckUsage[deck].losses += 1;
+      }
     }
   });
 
-  console.log("Les decks utilisé pour les 25 dernières matchs:", deckUsage);
+  console.log("Deck usage statistics:", deckUsage);
 
   // Transforme le dictionnaire en une liste triée des decks par leur utilisation (décroissante)
   const sortedDecks = Object.entries(deckUsage).sort(
     (a, b) => b[1].count - a[1].count
   );
 
-  //Joueur joue avec un seul deck pour tous les matchs
+  // Conditions :
+  // Si le joueur utilise un seul deck pour tous les matchs
   if (sortedDecks.length === 1) {
+    const deck = sortedDecks[0][1];
+    const winRate = (deck.wins / (deck.wins + deck.losses)) * 100;
     return {
       mostUsedDeck: sortedDecks[0][0],
-      elixir: sortedDecks[0][1].elixir,
+      elixir: deck.elixir,
+      winRate: winRate.toFixed(2) + "%",
+      count: deck.count, // Ajout du nombre de fois que le deck a été utilisé
     };
   }
 
-  //Le joueur joue un deck différent pour chaque match
+  // Si le joueur utilise un deck différent pour chaque match
   if (
     sortedDecks.length > 0 &&
     sortedDecks[0][1].count === 1 &&
@@ -48,23 +67,44 @@ const findMostUsedDeck = (battles) => {
   ) {
     return {
       message:
-        "Pas de deck le + utilisé pour ce joueur, svp essayez avec un autre joueur",
+        "Le joueur utilise un deck différent pour chaque match, veuillez essayer avec un autre joueur.",
     };
   }
 
-  //Il existe plus qu'un deck le plus utilisé
+  // Si plusieurs decks sont les plus utilisés
   const mostUsedDecks = sortedDecks.filter(
     (deck) => deck[1].count === sortedDecks[0][1].count
   );
   if (mostUsedDecks.length === 1) {
+    const deck = mostUsedDecks[0][1];
+    const winRate = (deck.wins / (deck.wins + deck.losses)) * 100;
     return {
       mostUsedDeck: mostUsedDecks[0][0],
-      elixir: mostUsedDecks[0][1].elixir,
+      elixir: deck.elixir,
+      winRate: winRate.toFixed(2) + "%",
+      count: deck.count, // Ajout du nombre de fois que le deck a été utilisé
     };
   }
-  //on choisit le deck le plus utilisé avec le coût moyen d'elixir le plus faible
+
+  // On choisit le deck le plus utilisé avec le coût moyen d'élixir le plus faible
   const bestDeck = mostUsedDecks.sort((a, b) => a[1].elixir - b[1].elixir)[0];
-  return { mostUsedDeck: bestDeck[0], elixir: bestDeck[1].elixir };
+  // On choisit le deck avec le meilleur taux de victoire
+  const bestWinRateDeck = mostUsedDecks.sort((a, b) => {
+    const winRateA = a[1].wins / (a[1].wins + a[1].losses);
+    const winRateB = b[1].wins / (b[1].wins + b[1].losses);
+    return winRateB - winRateA;
+  })[0];
+
+  const winRate =
+    (bestWinRateDeck[1].wins /
+      (bestWinRateDeck[1].wins + bestWinRateDeck[1].losses)) *
+    100;
+  return {
+    mostUsedDeck: bestWinRateDeck[0],
+    elixir: bestWinRateDeck[1].elixir,
+    winRate: winRate.toFixed(2) + "%",
+    count: bestWinRateDeck[1].count, // Ajout du nombre de fois que le deck a été utilisé
+  };
 };
 
 module.exports = { findMostUsedDeck };
